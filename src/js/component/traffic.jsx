2b26c97268f9f2e0481dc96284360060bc57c7ab
Bruno-Morais7 <brunomafonsomorais@gmail.com>
2022-04-17 13:53:58 +0000

    Bonus part done. Missing the purple in autoMode.

--- src/js/component/traffic.jsx
+++ src/js/component/traffic.jsx
@@ -2,25 +2,64 @@ import React, { useState } from "react";
 
 //create your first component
 const Traffic = () => {
-	const [selectedColor, setselectedColor] = useState();
+	const [selectedColor, setSelectedColor] = useState(null);
+	//	const [automode, setAutomode] = useState(false);
+	const [isPurpleOn, setIsPurpleOn] = useState(false);
+
+	let listOfLights = ["green", "red", "yellow"];
+	let i = 0;
 
 	return (
-		<div className="trafficLight">
-			<div
-				onClick={() => setselectedColor("red")}
-				className={
-					"light red" + (selectedColor === "red" ? " glow" : "")
-				}></div>
-			<div
-				onClick={() => setselectedColor("yellow")}
-				className={
-					"light yellow" + (selectedColor === "yellow" ? " glow" : "")
-				}></div>
-			<div
-				onClick={() => setselectedColor("green")}
-				className={
-					"light green" + (selectedColor === "green" ? " glow" : "")
-				}></div>
+		<div className="container col-4 text-center">
+			<div className="cable mx-auto"></div>
+			<div className="trafficLight d-inline-block">
+				<div
+					onClick={() => setSelectedColor("red")}
+					className={
+						"mb-2 light red" +
+						(selectedColor === "red" ? " glow" : "")
+					}></div>
+				<div
+					onClick={() => setSelectedColor("yellow")}
+					className={
+						"mb-2 light yellow" +
+						(selectedColor === "yellow" ? " glow" : "")
+					}></div>
+				<div
+					onClick={() => setSelectedColor("green")}
+					className={
+						"mb-2 light green" +
+						(selectedColor === "green" ? " glow" : "")
+					}></div>
+				{isPurpleOn === true ? (
+					<div
+						onClick={() => setSelectedColor("purple")}
+						className={
+							"mb-2 light purple" +
+							(selectedColor === "purple" ? " glow" : "")
+						}></div>
+				) : null}
+			</div>
+			<div>
+				<button
+					className="btn btn-secondary"
+					onClick={() =>
+						setInterval(function () {
+							i++;
+							let result = i % listOfLights.length;
+							let selectedLight = listOfLights[result] + " glow";
+							console.log(selectedLight);
+							setSelectedColor(listOfLights[result]);
+						}, 2000)
+					}>
+					Auto mode!
+				</button>
+				<button
+					className="btn btn-warning"
+					onClick={() => setIsPurpleOn(!isPurpleOn)}>
+					{isPurpleOn ? "Remove purple" : "Add purple!"}
+				</button>
+			</div>
 		</div>
 	);
 };
